71ff6ea0f24ce58a0fdb42499dd7390bb4fce004
Hanif <marbling1293@naver.com>
2020-09-15 13:17:09 +0900

    [ADD] auth Security

---
+++ src/main/java/honchi/api/global/config/security/AuthDetailsService.java
@@ -0,0 +1,22 @@
+package honchi.api.global.config.security;
+
+import honchi.api.domain.user.domain.repository.UserRepository;
+import honchi.api.global.error.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public AuthDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .map(AuthDetails::new)
+                .orElseThrow(UserNotFoundException::new);
+    }
+}
