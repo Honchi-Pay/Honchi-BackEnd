f08b5cb840db819a74eeabe2a4eb78705ee7eddd
Hanif <marbling1293@naver.com>
2020-10-04 01:39:01 +0900

    [ADD] findPassword Service

--- src/main/java/honchi/api/domain/user/service/UserService.java
+++ src/main/java/honchi/api/domain/user/service/UserService.java
@@ -7,6 +7,7 @@ public interface UserService {
     void join(SignUpRequest signUpRequest);
     void sendEmail(String email);
     void verifyEmail(VerifyCodeRequest verifyCodeRequest);
+    void findPassword(FindPasswordRequest findPasswordRequest);
     void chargePassword(ChargePasswordRequest chargePasswordRequest);
     ProfileResponse getProfile(Integer user_id);
     void updateProfile(ProfileUpdateRequest profileUpdateRequest);
