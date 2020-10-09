201b20ace206f4206b4ffb7de04adafe4e3404e6
Hanif <marbling1293@naver.com>
2020-10-09 15:45:22 +0900

    [ADD] Post Repository

---
+++ src/main/java/honchi/api/domain/post/domain/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package honchi.api.domain.post.domain.repository;
+
+import honchi.api.domain.post.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Integer> {
+}
