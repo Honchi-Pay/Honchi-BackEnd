2a235b09022ccc1e79469bdbad281b1171ed3156
Hanif <marbling1293@naver.com>
2020-10-14 14:56:38 +0900

    [ADD] Post List Controller

--- src/main/java/honchi/api/domain/post/controller/PostController.java
+++ src/main/java/honchi/api/domain/post/controller/PostController.java
@@ -1,12 +1,15 @@
 package honchi.api.domain.post.controller;
 
 import honchi.api.domain.post.dto.PostContentResponse;
+import honchi.api.domain.post.dto.PostListRequest;
+import honchi.api.domain.post.dto.PostListResponse;
 import honchi.api.domain.post.dto.PostWriteRequest;
 import honchi.api.domain.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/post")
@@ -18,6 +21,11 @@ public class PostController {
     @PostMapping
     public void write(@ModelAttribute @Valid PostWriteRequest postWriteRequest) {
         postService.write(postWriteRequest);
+    }
+
+    @GetMapping
+    public List<PostListResponse> getList(@RequestBody @Valid PostListRequest postListRequest) {
+        return postService.getList(postListRequest);
     }
 
     @GetMapping("/{postId}")
