db8fe6e5698e2a32a2f1d89132b153199385b2aa
Hanif <marbling1293@naver.com>
2020-10-16 09:00:56 +0900

    [ADD] search post title service

--- src/main/java/honchi/api/domain/post/service/PostService.java
+++ src/main/java/honchi/api/domain/post/service/PostService.java
@@ -8,6 +8,7 @@ public interface PostService {
 
     void write(PostWriteRequest postWriteRequest);
     List<PostListResponse> getList(PostListRequest postListRequest);
+    List<PostListResponse> getSearch(PostSearchListRequest postSearchListRequest);
     PostContentResponse getContent(Integer postId);
     void attendPost(Integer postId);
     void fixPost(Integer postId, PostFixRequest postFixRequest);
