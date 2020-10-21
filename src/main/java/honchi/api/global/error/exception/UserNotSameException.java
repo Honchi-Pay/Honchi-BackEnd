d1a46d2dcae9907b9bf6bede137e31f1c6402cba
Hanif <marbling1293@naver.com>
2020-10-21 21:56:50 +0900

    [DELETE] User Not Same Exception

--- src/main/java/honchi/api/global/error/exception/UserNotSameException.java
+++
@@ -1,8 +0,0 @@
-package honchi.api.global.error.exception;
-
-public class UserNotSameException extends BusinessException {
-
-    public UserNotSameException(){
-        super(ErrorCode.USER_NOT_SAME);
-    }
-}
