04e9ce04af93abe532bd1ece28e3c0505cd0e135
Hanif <marbling1293@naver.com>
2020-10-22 14:38:08 +0900

    [ADD] Delete By NickName

--- src/main/java/honchi/api/domain/user/domain/repository/UserRepository.java
+++ src/main/java/honchi/api/domain/user/domain/repository/UserRepository.java
@@ -11,4 +11,6 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
     Optional<User> findByNickName(String nickName);
     Optional<User> findByPhoneNumber(String phoneNumber);
+
+    void deleteByNickName(String nickName);
 }
