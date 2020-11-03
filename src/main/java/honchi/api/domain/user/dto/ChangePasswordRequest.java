d08aeb965294fcdf735a740006cab42001add24f
Hanif <marbling1293@naver.com>
2020-11-03 20:18:56 +0900

    [FIX] Delete @Setter

--- src/main/java/honchi/api/domain/user/dto/ChangePasswordRequest.java
+++ src/main/java/honchi/api/domain/user/dto/ChangePasswordRequest.java
@@ -1,12 +1,10 @@
 package honchi.api.domain.user.dto;
 
 import lombok.Getter;
-import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter
-@Setter
 public class ChangePasswordRequest {
 
     @NotBlank
--- src/main/java/honchi/api/domain/user/dto/SignUpRequest.java
+++ src/main/java/honchi/api/domain/user/dto/SignUpRequest.java
@@ -2,14 +2,12 @@ package honchi.api.domain.user.dto;
 
 import honchi.api.domain.user.domain.enums.Sex;
 import lombok.Getter;
-import lombok.Setter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Getter
-@Setter
 public class SignUpRequest {
 
     @Email
