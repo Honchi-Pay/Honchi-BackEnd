e77d9e1bd3b9e930e3005f5c711bafcf3f0849fa
Hanif <marbling1293@naver.com>
2020-11-03 20:17:03 +0900

    [FIX] allowed Method

--- src/main/java/honchi/api/global/config/CorsConfig.java
+++ src/main/java/honchi/api/global/config/CorsConfig.java
@@ -14,6 +14,6 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedHeaders("*")
-                .allowedMethods("*");
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
