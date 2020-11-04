3286ce6fc2eeefd669a0688a56076405d2bb36c8
Hanif <marbling1293@naver.com>
2020-11-04 10:56:46 +0900

    [FIX] Delete @Setter

--- src/main/java/honchi/api/domain/post/dto/PostListRequest.java
+++ src/main/java/honchi/api/domain/post/dto/PostListRequest.java
@@ -1,12 +1,10 @@
 package honchi.api.domain.post.dto;
 
 import lombok.Getter;
-import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
 
 @Getter
-@Setter
 public class PostListRequest {
 
     private double lon;
