8e5205b4f1359a3535a51a00c93817438e4c2262
Hanif <marbling1293@naver.com>
2020-11-13 09:05:31 +0900

    [ADD] chatRepository

--- src/main/java/honchi/api/domain/chat/domain/repository/ChatRepository.java
+++ src/main/java/honchi/api/domain/chat/domain/repository/ChatRepository.java
@@ -6,11 +6,15 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ChatRepository extends CrudRepository<Chat, Integer> {
 
     List<Chat> findAllByUserId(Integer userId);
+    List<Chat> findAllByRoomId(String roomId);
+    Optional<Chat> findByRoomId(String roomId);
     Chat findByRoomIdAndAuthority(String roomId, Authority authority);
     Integer countByRoomId(String roomId);
+    void deleteByUserIdAndRoomId(Integer userId, String roomId);
 }
