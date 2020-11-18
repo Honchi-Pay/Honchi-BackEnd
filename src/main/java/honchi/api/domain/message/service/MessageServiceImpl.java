37925bb8fe7cc666c3a4171143d73027acebda8c
Hanif <marbling1293@naver.com>
2020-11-18 20:24:09 +0900

    [ADD] sendImage Service

--- src/main/java/honchi/api/domain/message/service/MessageServiceImpl.java
+++ src/main/java/honchi/api/domain/message/service/MessageServiceImpl.java
@@ -1,7 +1,11 @@
 package honchi.api.domain.message.service;
 
+import honchi.api.domain.chat.domain.repository.ChatRepository;
+import honchi.api.domain.chat.exception.ChatNotFoundException;
 import honchi.api.domain.message.domain.Message;
+import honchi.api.domain.message.domain.enums.MessageType;
 import honchi.api.domain.message.domain.repository.MessageRepository;
+import honchi.api.domain.message.dto.ImageRequest;
 import honchi.api.domain.message.dto.MessageRequest;
 import honchi.api.domain.message.dto.MessageResponse;
 import honchi.api.domain.user.domain.User;
@@ -9,19 +13,54 @@ import honchi.api.domain.user.domain.repository.UserRepository;
 import honchi.api.global.config.security.AuthenticationFacade;
 import honchi.api.global.error.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class MessageServiceImpl implements MessageService {
 
     private final UserRepository userRepository;
+    private final ChatRepository chatRepository;
     private final MessageRepository messageRepository;
 
     private final AuthenticationFacade authenticationFacade;
+
+    @Value("${image.upload.dir}")
+    private String imageDirPath;
+
+    @SneakyThrows
+    @Override
+    public void sendImage(ImageRequest imageRequest) {
+        User user = userRepository.findByEmail(authenticationFacade.getUserEmail())
+                .orElseThrow(UserNotFoundException::new);
+
+        chatRepository.findById(imageRequest.getChatId())
+                .orElseThrow(ChatNotFoundException::new);
+
+        String imageName = UUID.randomUUID().toString();
+
+        messageRepository.save(
+                Message.builder()
+                        .chatId(imageRequest.getChatId())
+                        .userId(user.getId())
+                        .message(imageName)
+                        .messageType(MessageType.IMAGE)
+                        .isDelete(false)
+                        .isShow(false)
+                        .time(LocalDateTime.now())
+                        .build()
+        );
+
+        imageRequest.getImage().transferTo(new File(imageDirPath, imageName));
+    }
 
     @Override
     public List<MessageResponse> getList(MessageRequest messageRequest) {
@@ -30,7 +69,7 @@ public class MessageServiceImpl implements MessageService {
 
         List<MessageResponse> messages = new ArrayList<>();
 
-        for (Message message : messageRepository.findAllByRoomIdOrderByTimeDesc(messageRequest.getRoomId())) {
+        for (Message message : messageRepository.findAllByChatIdOrderByTimeDesc(messageRequest.getChatId())) {
             User user = userRepository.findById(message.getId())
                     .orElseThrow(UserNotFoundException::new);
 
