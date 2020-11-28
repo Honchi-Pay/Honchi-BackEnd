00025e605457863befabc383b8881b0e734659b3
Hanif <marbling1293@naver.com>
2020-11-29 01:15:08 +0900

    [ADD] getBuyContent Service

--- src/main/java/honchi/api/domain/buyList/service/BuyListServiceImpl.java
+++ src/main/java/honchi/api/domain/buyList/service/BuyListServiceImpl.java
@@ -2,11 +2,19 @@ package honchi.api.domain.buyList.service;
 
 import honchi.api.domain.buyList.domain.BuyList;
 import honchi.api.domain.buyList.domain.repository.BuyListRepository;
+import honchi.api.domain.buyList.dto.BuyContentResponse;
 import honchi.api.domain.buyList.dto.BuyListResponse;
+import honchi.api.domain.buyList.exception.BuyListNotFoundException;
 import honchi.api.domain.post.domain.Post;
+import honchi.api.domain.post.domain.PostAttend;
+import honchi.api.domain.post.domain.repository.PostAttendRepository;
+import honchi.api.domain.post.domain.repository.PostImageRepository;
 import honchi.api.domain.post.domain.repository.PostRepository;
+import honchi.api.domain.post.dto.PostAttendListResponse;
 import honchi.api.domain.post.exception.PostNotFoundException;
 import honchi.api.domain.user.domain.User;
+import honchi.api.domain.user.domain.UserImage;
+import honchi.api.domain.user.domain.repository.UserImageRepository;
 import honchi.api.domain.user.domain.repository.UserRepository;
 import honchi.api.global.config.security.AuthenticationFacade;
 import honchi.api.global.error.exception.UserNotFoundException;
@@ -23,6 +31,9 @@ public class BuyListServiceImpl implements BuyListService {
     private final UserRepository userRepository;
     private final PostRepository postRepository;
     private final BuyListRepository buyListRepository;
+    private final UserImageRepository userImageRepository;
+    private final PostImageRepository postImageRepository;
+    private final PostAttendRepository postAttendRepository;
 
     private final AuthenticationFacade authenticationFacade;
 
@@ -48,5 +59,50 @@ public class BuyListServiceImpl implements BuyListService {
         }
 
         return buyListResponses;
+    }
+
+    @Override
+    public List<BuyContentResponse> getContent(Integer buyId) {
+        userRepository.findByEmail(authenticationFacade.getUserEmail())
+                .orElseThrow(UserNotFoundException::new);
+
+        List<BuyContentResponse> buyContentResponses = new ArrayList<>();
+
+        BuyList buyList = buyListRepository.findById(buyId)
+                .orElseThrow(BuyListNotFoundException::new);
+
+        Post post = postRepository.findById(buyList.getPostId())
+                .orElseThrow(PostNotFoundException::new);
+
+        List<String> postImages = new ArrayList<>();
+        postImageRepository.findAllByPostId(post.getId())
+                .forEach(postImage -> postImages.add(postImage.getImageName()));
+
+        List<PostAttendListResponse> postAttends = new ArrayList<>();
+        for (PostAttend postAttend : postAttendRepository.findByPostId(post.getId())) {
+            User user = userRepository.findById(postAttend.getUserId())
+                    .orElseThrow(UserNotFoundException::new);
+
+            UserImage userImage = userImageRepository.findByUserId(user.getId());
+
+            postAttends.add(
+                    PostAttendListResponse.builder()
+                            .nickName(user.getNickName())
+                            .image(userImage == null ? null : userImage.getImageName())
+                            .build()
+            );
+        }
+
+        buyContentResponses.add(
+                BuyContentResponse.builder()
+                        .title(post.getTitle())
+                        .price(buyList.getPrice())
+                        .time(buyList.getTime())
+                        .images(postImages)
+                        .attendList(postAttends)
+                        .build()
+        );
+
+        return buyContentResponses;
     }
 }
