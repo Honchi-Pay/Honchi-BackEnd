b5e0fc7228210580d2af6070b3d93d6d36332d92
Hanif <marbling1293@naver.com>
2020-11-28 16:09:16 +0900

    [ADD] @Qurey

--- src/main/java/honchi/api/domain/message/domain/repository/MessageRepository.java
+++ src/main/java/honchi/api/domain/message/domain/repository/MessageRepository.java
@@ -1,7 +1,9 @@
 package honchi.api.domain.message.domain.repository;
 
 import honchi.api.domain.message.domain.Message;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +13,7 @@ public interface MessageRepository extends CrudRepository<Message, Integer> {
 
     List<Message> findAllByChatIdOrderByTimeDesc(String chatId);
     Message findTop1ByChatIdOrderByTimeDesc(String chatId);
+
+    @Query(value = "select * from message where chat_id = :chatId and id > :a and id <= :b", nativeQuery = true)
+    List<Message> findByChatIdAndIdAndId(@Param("chatId") String chatId, @Param("a") Integer a, @Param("b") Integer b);
 }
