26305aaca5a8438d197fd7d20becdab2cdf5722d
Hanif <marbling1293@naver.com>
2020-11-29 01:14:33 +0900

    [ADD] getBuyList Content

--- src/main/java/honchi/api/domain/buyList/controller/BuyListController.java
+++ src/main/java/honchi/api/domain/buyList/controller/BuyListController.java
@@ -1,12 +1,15 @@
 package honchi.api.domain.buyList.controller;
 
+import honchi.api.domain.buyList.dto.BuyContentResponse;
 import honchi.api.domain.buyList.dto.BuyListResponse;
 import honchi.api.domain.buyList.service.BuyListService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -19,5 +22,10 @@ public class BuyListController {
     @GetMapping
     public List<BuyListResponse> getBuyList() {
         return buyListService.getBuyList();
+    }
+
+    @GetMapping("/{buyId}")
+    public List<BuyContentResponse> getContent(@PathVariable @Valid Integer buyId) {
+        return buyListService.getContent(buyId);
     }
 }
