56238eecf50ed5ff9f4333ed4fc0be2e86679aed
Hanif <marbling1293@naver.com>
2020-12-03 10:01:51 +0900

    [FIX] image load

--- src/main/java/honchi/api/domain/image/service/ImageServiceImpl.java
+++ src/main/java/honchi/api/domain/image/service/ImageServiceImpl.java
@@ -1,27 +1,29 @@
 package honchi.api.domain.image.service;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.amazonaws.util.IOUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-
 @Service
 @RequiredArgsConstructor
 public class ImageServiceImpl implements ImageService {
 
-    @Value("${image.upload.dir}")
-    private String imageDirPath;
+    private final AmazonS3 s3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
 
     @SneakyThrows
     @Override
     public byte[] getImage(String imageName) {
-        InputStream inputStream = new FileInputStream(new File(imageDirPath, imageName));
+        S3Object s3Object = s3Client.getObject(bucket, imageName);
+        S3ObjectInputStream stream = s3Object.getObjectContent();
 
-        return IOUtils.toByteArray(inputStream);
+        return IOUtils.toByteArray(stream);
     }
 }
